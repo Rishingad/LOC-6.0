280382269c8ac9ec33ac9d9f1a654ddef06779b6
Rihansh Hingad <99811062+Rishingad@users.noreply.github.com>
2023-10-27 20:45:26 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar/Navbar";
 import HomePage from "../components/Home/Home";
 import SocialMediaSideBar from "../components/SocialMediaSideBar/SocialMediaSideBar";
 import Carousel from "../components/Carousel/Carousel";
-import AboutUs from "../components/AboutUs/AboutUs";
+import AboutUs from "../components/AboutUs/Aboutus";
 import FAQ from "../components/FAQ/FAQ";
 import Contact from "../components/Contact/Contact";
 import { redirect } from "next/navigation";
