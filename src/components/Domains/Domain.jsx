c585e2e7325d48de282b88767d7cefa7c18701a6
mitshah2406 <mitshah2406@gmail.com>
2024-02-24 23:33:17 +0530

    Domain section done

--- src/components/Domains/Domain.jsx
+++ src/components/Domains/Domain.jsx
@@ -1,7 +1,68 @@
-
+import './Domain.css'
 const Domain = () => {
     return (
-        <div>Domain</div>
+        <>
+            <div className="domain-wrapper">
+                <div className="domain-heading">Domain</div>
+
+                <div className="domain-content">
+                    {/* <div className="domain-card">
+                        <div className="content">
+                            <div className="domain-front">
+                                <img src="../../../public/entering_area_51.png" alt="" />
+                            </div>
+                            <div className="domain-back">
+                                <img src="../../../public/Domains/web.png" alt="" />
+                                <p>Web Dev</p>
+                            </div>
+                        </div>
+                    </div> */}
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/web.png" alt="" />
+                                <p>Web Dev</p></div>
+                        </div>
+                    </div>
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/app.png" alt="" />
+                                <p>App Dev</p></div>
+                        </div>
+                    </div>
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/aiml.png" alt="" />
+                                <p>AI/ML</p></div>
+                        </div>
+                    </div>
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/blockchain.png" alt="" />
+                                <p>Blockchain</p></div>
+                        </div>
+                    </div>
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/iot.png" alt="" />
+                                <p>IoT</p></div>
+                        </div>
+                    </div>
+                    <div className="domain-container">
+                        <div className="domain-card">
+                            <div className="domain-front"> <img src="../../../public/entering_area_51.png" alt="" /></div>
+                            <div className="domain-back"> <img src="../../../public/Domains/openinnovation.png" alt="" />
+                                <p>Open Innovation</p></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,14 @@
 import Home from '../components/Home/Home'
 import Navbar from '../components/Navbar/Navbar'
 import Sponsors from '../components/Sponsors'
-
+import Domain from '../components/Domains/Domain'
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <Home />
       <Sponsors></Sponsors>
+      <Domain></Domain>
     </>
   )
 }
