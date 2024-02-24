b60dca20b29f20d0f3eaba2b68065fe039be0fc2
mitshah2406 <mitshah2406@gmail.com>
2024-02-24 19:02:39 +0530

    misc

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
 
 const HomePage = () => {
-  return <div>Homepage</div>;
+  return <>
+    <nav className="bg-black flex justify-between align-middle w-full left-0 right-0 top-0 fixed navbar py-6 px-8">
+      <div className="logo">
+        <img src="/logo/logo.png" alt="" width={70} height={70} />
+      </div>
+
+      <ul className="text-white flex justify-between align-middle gap-x-2 menu-items w-96">
+        <li><a className="font-bold font-sans" href="/">Home</a></li>
+        <li><a className="font-bold font-sans" href="/sponsors">Sponsors</a></li>
+        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
+        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
+        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
+        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
+      </ul>
+
+    </nav>
+  </>;
 };
 
 export default HomePage;
