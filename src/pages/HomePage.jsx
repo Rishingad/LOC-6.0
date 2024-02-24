2699bf8c4c8a2435cec2b727f45125ca0eaf4a65
mitshah2406 <mitshah2406@gmail.com>
2024-02-24 21:08:25 +0530

    Home page done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,15 @@
 
-const HomePage = () => {
-  return <>
-    <nav className="bg-black flex justify-between align-middle w-full left-0 right-0 top-0 fixed navbar py-6 px-8">
-      <div className="logo">
-        <img src="/logo/logo.png" alt="" width={70} height={70} />
-      </div>
 
-      <ul className="text-white flex justify-between align-middle gap-x-2 menu-items w-96">
-        <li><a className="font-bold font-sans" href="/">Home</a></li>
-        <li><a className="font-bold font-sans" href="/sponsors">Sponsors</a></li>
-        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
-        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
-        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
-        <li><a className="font-bold font-sans" href="/faq">FAQ</a></li>
-      </ul>
+import Home from '../components/Home/Home'
+import Navbar from '../components/Navbar/Navbar'
+import Sponsors from '../components/Sponsors'
 
-    </nav>
-  </>;
-};
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Sponsors></Sponsors>
+    </>
+  )
+}
