711194062a5eae2bcc760d252bfc1ccdc0753b9c
mitshah2406 <mitshah2406@gmail.com>
2024-02-25 13:46:56 +0530

    Devfolio button added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,17 @@
 import './Home.css'
+import { useEffect } from 'react';
 const Home = () => {
 
-    // useEffect(() => {
-    //     const script = document.createElement('script');
-    //     script.src = 'https://apply.devfolio.co/v2/sdk.js';
-    //     script.async = true;
-    //     script.defer = true;
-    //     document.body.appendChild(script);
-    //     return () => {
-    //         document.body.removeChild(script);
-    //     }
-    // }, []);
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://apply.devfolio.co/v2/sdk.js';
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
     return (
         <>
             <div className="home-wrapper">
@@ -21,14 +22,14 @@ const Home = () => {
                     <div className="subtitle-wrapper">
                         A 24 hour National level hackathon conducted in hybrid mode organized by Dwarkadas J Sanghvi College of Engineering’s ACM Student Chapter
                     </div>
-                    <div className="devfolio_wrapper">
-                        {/* <div
-                            className="apply-button"
-                            data-hackathon-slug="lines-of-code-djsce"
-                            data-button-theme="light"
 
-                        ></div> */}
-                    </div>
+                    <div
+                        className="apply-button"
+                        data-hackathon-slug={"lines-of-code-djsce"}
+                        data-button-theme={"light"}
+                        style={{ height: '44px', width: '312px' }}
+                    ></div>
+
                 </div>
 
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 
-
+// import { useEffect } from 'react'
 import Home from '../components/Home/Home'
 import Navbar from '../components/Navbar/Navbar'
 import Sponsors from '../components/Sponsors'
 import Domain from '../components/Domains/Domain'
 export default function HomePage() {
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.src = 'https://apply.devfolio.co/v2/sdk.js';
+  //   script.async = true;
+  //   script.defer = true;
+  //   document.body.appendChild(script);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   }
+  // }, []);
   return (
     <>
       <Navbar />
