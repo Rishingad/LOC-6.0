00207704d16cd70956670b9a88071880c90b6852
YashLoriya02 <143203752+YashLoriya02@users.noreply.github.com>
2024-02-29 23:31:09 +0530

    sponsors section changed

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -6,6 +6,10 @@ import bg from "/sponsor/bg_sponsor.png";
 import devfolio from "/sponsor/devfolio.png";
 import eth from "/sponsor/eth.png";
 import polygon from "/sponsor/polygon.png";
+import dubai from "../../assets/dubai.png"
+import educat from "../../assets/educat.jpg"
+import Nutorio from "../../assets/Nutorio.jpg"
+import './Sponsors.css'
 
 const Sponsors = () => {
   useEffect(() => {
@@ -14,10 +18,21 @@ const Sponsors = () => {
       easing: "ease-in-out",
     });
   }, []);
+
+  const getBottomSpacing = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 1100) {
+      return "4%";
+    }
+    else if (screenWidth < 1100) {
+      return "1%";
+    }
+  };
+
   return (
     <>
       <section className=" bg-[#040B11] w-full  md:h-auto relative overflow-hidden ">
-        <div className=" relative w-full flex justify-center items-center">
+        <div className="custom_img_container relative w-full flex justify-center items-center">
           <p
             data-aos="fade-right"
             className="absolute font-bakbak w-full top-[1rem] text-center font-semibold text-[3rem] md:text-[5rem] text-white z-20"
@@ -27,24 +42,53 @@ const Sponsors = () => {
           <img
             src={devfolio}
             data-aos="fade-down"
-            className=" absolute w-[50%] sm:w-[25%] top-[30%] sm:top-[40%]    z-20"
+            className="img_class devfolio absolute w-[50%] sm:w-[25%] sm:top-[30%] z-20"
             alt="sponsors2"
           />
           <img
             src={eth}
             data-aos="fade-right"
-            className=" absolute w-[50%] sm:w-[25%] bottom-[32%]  sm:left-[10%] sm:bottom-[10%]  z-20"
+            className="img_class eth absolute w-[50%] sm:w-[25%] sm:left-[10%] sm:top-[30%] z-20"
             alt="sponsors1"
           />
           <img
+            src={dubai}
+            data-aos="fade-right"
+            className="img_class dubai absolute w-[50%] sm:w-[25%] sm:bottom-[10%]  z-20"
+            alt="sponsors1"
+          />
+          <div style={{ bottom: getBottomSpacing() }} className="partner_text font-bakbak absolute w-[50%] sm:w-[25%] sm:bottom-[4%] z-20">
+            Energizing partner
+          </div>
+          <img
+            src={Nutorio}
+            data-aos="fade-right"
+            // className=" absolute w-[50%] sm:w-[25%] bottom-[32%] sm:left-[10%] sm:bottom-[10%]  z-20"
+            className="img_class nutorio absolute w-[50%] sm:w-[25%] sm:left-[10%] sm:bottom-[10%] z-20"
+            alt="sponsors1"
+          />
+          <div style={{ bottom: getBottomSpacing() }} className="partner_text font-bakbak absolute w-[50%] sm:w-[25%] sm:bottom-[4%] sm:left-[10%] z-20">
+            Healthy snacking partner
+          </div>
+          <img
+            src={educat}
+            data-aos="fade-right"
+            // className=" absolute w-[50%] sm:w-[25%] bottom-[32%]  sm:left-[10%] sm:bottom-[10%]  z-20"
+            className="img_class educat absolute w-[50%] sm:w-[25%] z-20 sm:right-[10%] sm:bottom-[10%]"
+            alt="sponsors1"
+          />
+          <div style={{ bottom: getBottomSpacing() }} className="partner_text font-bakbak absolute w-[50%] sm:w-[25%] sm:bottom-[4%] sm:right-[10%]  z-20">
+            Education partner
+          </div>
+          <img
             src={polygon}
             data-aos="fade-left"
-            className=" absolute w-[50%] sm:w-[25%] z-20 sm:right-[10%] bottom-[10%]"
+            className="img_class polygon absolute w-[50%] sm:w-[25%] z-20 sm:right-[10%] sm:top-[30%]"
             alt="sponsors3"
           />
           <img
             src={bg}
-            className=" w-full h-[60vh] lg:h-[100vh] z-10"
+            className=" w-full h-[100vh] lg:h-[100vh] z-10"
             alt="bg"
           />
           {/* <img
